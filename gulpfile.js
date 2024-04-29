7c71d02f9a28ab161849fc11f1c3c44de7696cac
iHeyTang <dehui1012@qq.com>
2024-04-29 22:21:05 +0800

    fix: path is not defined

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 'use strict';
+const path = require('path');
+
 const tsconfigPath = path.join(__dirname, 'tools/gulp/tsconfig.json');
 require('ts-node').register({ project: tsconfigPath });
 require('./tools/gulp');
